7faa5e3bd85a53c627273874f173793e2957f399
shima-p-ashok <shimapashok29@gmail.com>
2025-08-25 00:26:57 +0530

    success updation

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -25,37 +25,55 @@ function LandingPage() {
   return (
     <div>
       <section id="bg">
-       
         <video autoPlay muted loop playsInline id="bg-video">
           <source src="https://cdn.pixabay.com/video/2024/05/29/214409_tiny.mp4" type="video/mp4" />
           Your browser does not support HTML5 video.
         </video>
 
-        
-        <div className="overlay">
-          <div className="row">
-            <div className="col-4"></div>
-            <div className="col-4 shadow border rounded text-center p-3 my-5 bg-light bg-opacity-50" >
-              <h4>Check the Weather</h4>
-              <label className='pt-4'>City : </label>
-              <input onChange={location} type="text" placeholder='Enter city name' /><br /><br />
-              <Button onClick={btnclick} sx={{ backgroundColor: 'oklch(37.9% 0.146 265.522)' }} variant="contained">Check Weather</Button><br /><br />
-              {wthr && wthr.weather && (
-                <div className="p-3">
-                  <img
-                    src={`https://openweathermap.org/img/wn/${wthr.weather[0].icon}@2x.png`}
-                    alt={wthr.weather[0].description}
-                    style={{ width: '80px', height: '80px' }}
-                  />
-                  <h4>Location : {wthr.name}, {wthr.sys.country}</h4>
-                  <h5>Weather: {wthr.weather[0].description}</h5>
-                  <h5>Temperature: {wthr.main.temp} °C</h5>
-                  <h5>Humidity: {wthr.main.humidity} %</h5>
-                  <h5>Wind speed: {wthr.wind.speed} kph</h5>
+       
+        <div className="overlay d-flex justify-content-center align-items-center min-vh-100">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-12 col-md-8 col-lg-6">
+                <div className="shadow border rounded text-center p-4 bg-light bg-opacity-75">
+                  <h4 className="mb-3">Check the Weather</h4>
+
+                  <div className="mb-3 text-start">
+                    <label className="form-label">City :</label>
+                    <input 
+                      onChange={location} 
+                      type="text" 
+                      className="form-control" 
+                      placeholder="Enter city name" 
+                    />
+                  </div>
+
+                  <Button 
+                    onClick={btnclick} 
+                    sx={{ backgroundColor: 'oklch(37.9% 0.146 265.522)' }} 
+                    variant="contained" 
+                    fullWidth
+                  >
+                    Check Weather
+                  </Button>
+
+                  {wthr && wthr.weather && (
+                    <div className="mt-4">
+                      <img
+                        src={`https://openweathermap.org/img/wn/${wthr.weather[0].icon}@2x.png`}
+                        alt={wthr.weather[0].description}
+                        style={{ width: '80px', height: '80px' }}
+                      />
+                      <h4>Location : {wthr.name}, {wthr.sys.country}</h4>
+                      <h5>Weather: {wthr.weather[0].description}</h5>
+                      <h5>Temperature: {wthr.main.temp} °C</h5>
+                      <h5>Humidity: {wthr.main.humidity} %</h5>
+                      <h5>Wind speed: {wthr.wind.speed} kph</h5>
+                    </div>
+                  )}
                 </div>
-              )}
+              </div>
             </div>
-            <div className="col-4"></div>
           </div>
         </div>
       </section>
